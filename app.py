71a7035e3666352d184938041e2ab35826628790
Maciek Walega <mwaleg.sa@gmail.com>
2021-02-21 03:01:58 +0100

    change to ImageNet

--- app.py
+++ app.py
@@ -1,45 +1,52 @@
 import os
 from flask import Flask, render_template, request
-import pickle
 from PIL import Image
 from torchvision.transforms import transforms
+import torchvision.models as models
 import torch
+import torch.nn.functional
 
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'C:/Users/mwale/PycharmProjects/resnet_app/static'
-DEVICE = 'cuda'
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 MODEL = None
 
 
 def get_class(pred):
-    classes = {
-        0: 'airplane',
-        1: 'automobile',
-        2: 'bird',
-        3: 'cat',
-        4: 'deer',
-        5: 'dog',
-        6: 'frog',
-        7: 'horse',
-        8: 'ship',
-        9: 'truck'
-    }
+    with open("imagenet_classes.txt", "r") as f:
+        categories = [s.strip() for s in f.readlines()]
 
-    return classes[torch.argmax(pred).item()]
+    # Show top categories per image
+    top3_prob, top3_catid = torch.topk(pred, 3)
+    top_3 = []
+    print(torch.max(pred))
+    for i in range(top3_prob.size(0)):
+        top_3.append((categories[top3_catid[i]], "{:.2%}".format(top3_prob[i].item())))
+
+    return top_3
 
 def predict(image_path, model):
     # open and transform image from path
     img = Image.open(image_path)
-    img = transforms.ToTensor()(img).unsqueeze(0)
-    img = transforms.Resize(320)(img)
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    img = preprocess(img)
+    img = img.unsqueeze(0)
     img = img.to(DEVICE)
 
     # predict
-    pred = model(img)
+    with torch.no_grad():
+        out = model(img)
+
+    out = torch.nn.functional.softmax(out[0], dim=0)
 
     # get correct class
-    ans = get_class(pred)
+    ans = get_class(out)
 
     return ans
 
@@ -53,12 +60,12 @@ def upload_predict():
                 image_file.filename
             )
             image_file.save(image_location)
-            pred = predict(image_location, MODEL)
-            return render_template('index.html', prediction=pred, image_name=image_file.filename)
+            preds = predict(image_location, MODEL)
+            return render_template('index.html', prediction=preds, image_name=image_file.filename)
     return render_template('index.html', prediction=0, image_name=None)
 
 if __name__ == '__main__':
-    pickle_in = open('resnet18_pretrained.pkl', 'rb')
-    MODEL = pickle.load(pickle_in)
+    MODEL = models.resnet18(pretrained=True)
+    MODEL.eval()
     MODEL.to(DEVICE)
     app.run(debug=True)
